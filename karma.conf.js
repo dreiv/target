3279c2d105b8106af19d624b871ef2dd9395ff4d
Andrei Voicu <dreivoicu@gmail.com>
2018-04-03 03:32:37 +0300

    added firefox headless single run test mode

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
@@ -27,13 +28,20 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'Firefox'],
     singleRun: false,
     customLaunchers: {
       ChromeHeadlessCI: {
         base: "ChromeHeadless",
         flags: ["--no-sandbox"]
-      }
+      },
+      browsers: ['FirefoxHeadless'],
+      customLaunchers: {
+        FirefoxHeadless: {
+          base: 'Firefox',
+          flags: [ '-headless' ],
+        },
+      },
     },
     browserNoActivityTimeout: 60000
   });
